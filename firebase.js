08dbe90d8d3ca50f7e23325fbdeae637d37d2a52
yisroel613770 <76762765+yisroel613770@users.noreply.github.com>
2025-01-28 18:53:07 +1100

    Get

--- firebase.js
+++ firebase.js
@@ -1,20 +1,19 @@
-// Import Firebase modules
+// Import Firebase libraries
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
-// Firebase configuration
+// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBoi_pOuLAc26iRz--YNuVkogcfddOPcOM",
-  authDomain: "jmovies-513c7.firebaseapp.com",
-  projectId: "jmovies-513c7",
-  storageBucket: "jmovies-513c7.appspot.com",
-  messagingSenderId: "147745857096",
-  appId: "1:147745857096:web:2a68928e0e62e9b9adc27d",
+    apiKey: "AIzaSyBoi_pOuLAc26iRz--YNuVkogcfddOPcOM",
+    authDomain: "jmovies-513c7.firebaseapp.com",
+    projectId: "jmovies-513c7",
+    storageBucket: "jmovies-513c7.appspot.com",
+    messagingSenderId: "147745857096",
+    appId: "1:147745857096:web:2a68928e0e62e9b9adc27d"
 };
 
-// Initialize Firebase and Firestore
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-// Export the Firestore database instance for use in other files
-export { db };
+// Initialize Firestore
+export const db = getFirestore(app);
